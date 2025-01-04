065ccd14a5166a20dba4d5dce08bee205f775306
devi9113624552 <dpraidola@gmail.com>
2025-01-04 07:12:56 +0530

    Added examples for error handling

--- error-playground.js
+++ error-playground.js
@@ -9,4 +9,12 @@ console.log(sum(1));
 
 //Handling errors
 //For synchronous code we can wrap it around try and catch blocks
+// try {
+//   sum(1);
+// } catch (error) {
+//   console.log("some error occured:", error);
+// }
 //For asynchronous code we can use then and catch for that
+// sum(1)
+//   .then((res) => console.log("success"))
+//   .catch((err) => console.log("we got an error"));
