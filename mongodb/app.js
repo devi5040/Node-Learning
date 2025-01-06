0ace2abf10615bd695efa9194d359f91c57824c8
devi9113624552 <dpraidola@gmail.com>
2025-01-06 21:56:26 +0530

    added functionality for editing, deleting and updating product and updated user model as well

--- mongodb/app.js
+++ mongodb/app.js
@@ -6,29 +6,30 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
 
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("677c00381139185c3f7fbae0")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -1,16 +1,45 @@
-const Sequelize = require('sequelize');
+const { getDb } = require("../util/database");
+const mongodb = require("mongodb");
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email, cart, id) {
+    this.username = username;
+    this.email = email;
+    this.cart = cart; //{items:[]}
+    this._id = id;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((result) => console.log("created"))
+      .catch((err) => console.log(err));
+  }
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((error) => console.log(error));
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
+  }
+}
 
 module.exports = User;
