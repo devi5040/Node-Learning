325d5917d1225a7e8732d14f847547199edd1fc3
devi9113624552 <dpraidola@gmail.com>
2025-01-23 21:45:59 +0530

    Added server side validations for adding post request

--- Rest-Api-Project/backend/constrollers/feed.js
+++ Rest-Api-Project/backend/constrollers/feed.js
@@ -1,3 +1,5 @@
+const validationResult = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,15 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const error = validationResult(req);
+  if (error.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is invalid",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
--- Rest-Api-Project/backend/routes/feed.js
+++ Rest-Api-Project/backend/routes/feed.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const feedController = require("../constrollers/feed");
 const router = express.Router();
+const body = require("express-validator");
 
 router.get("/posts", feedController.getPosts);
-router.post("/post", feedController.createPost);
+//validation for post request is added
+router.post(
+  "/post",
+  body("title").trim().isLength({ min: 5 }),
+  body("content").trim().isLength({ min: 5 }),
+  feedController.createPost
+);
 
 module.exports = router;
