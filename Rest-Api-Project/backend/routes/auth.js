9f4199ba9b9bfdba0e02b31e7597b32e6a7d1891
devi9113624552 <dpraidola@gmail.com>
2025-01-26 22:11:09 +0530

    Added signup action

--- Rest-Api-Project/backend/routes/auth.js
+++ Rest-Api-Project/backend/routes/auth.js
@@ -1,7 +1,27 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const router = express.Router();
+const User = require("../models/user");
+const authController = require("../constrollers/auth");
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    check("password").trim().isLength({ min: 5 }),
+    check("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
